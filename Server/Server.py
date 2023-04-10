6c27d6f29c94d7fcc8fc4fc3f33a4fee024b82b3
Mahas1 <mahasvan@gmail.com>
2023-04-10 21:18:10 +0530

    WIP - receive data in chunks and append until connection closed

--- Server/Server.py
+++ Server/Server.py
@@ -26,30 +26,35 @@ import struct
 
 
 async def handler(websocket):
+    data = []
     while True:
-        data = await websocket.recv()
-        print(data)
+        try:
+            data_chunk = await websocket.recv()
+        except websockets.exceptions.ConnectionClosed:
+            break
+        print(data_chunk)
+        data.append(data_chunk)
         # pprint.pprint(data)
-        with open("data.bin", "wb") as f:
-            f.write(data)
-        channels = 1
-        sample_width = 2  # 16-bit
-        frame_rate = 44100
-        n_frames = 200  # number of frames to write
-        comptype = "NONE"
-        compname = "not compressed"
+    with open("data.bin", "wb") as f:
+        f.write(data)
+    channels = 1
+    sample_width = 2  # 16-bit
+    frame_rate = 44100
+    n_frames = 200  # number of frames to write
+    comptype = "NONE"
+    compname = "not compressed"
 
-        with wave.open("output.wav", "w") as wav_file:
-            wav_file.setparams(
-                (channels, sample_width, frame_rate, n_frames, comptype, compname))
-            with open("data.bin", "rb") as bin_file:
-                data = bin_file.read(2)
-                while data:
-                    # convert the 2 bytes to a signed 16-bit integer
-                    sample = struct.unpack("<h", data)[0]
-                    # write the sample to the WAV file
-                    wav_file.writeframesraw(struct.pack("<h", sample))
-                    data = bin_file.read(2)
+    with wave.open("output.wav", "w") as wav_file:
+        wav_file.setparams(
+            (channels, sample_width, frame_rate, n_frames, comptype, compname))
+        with open("data.bin", "rb") as bin_file:
+            data_chunk = bin_file.read(2)
+            while data_chunk:
+                # convert the 2 bytes to a signed 16-bit integer
+                sample = struct.unpack("<h", data_chunk)[0]
+                # write the sample to the WAV file
+                wav_file.writeframesraw(struct.pack("<h", sample))
+                data_chunk = bin_file.read(2)
 
 
 async def main():
